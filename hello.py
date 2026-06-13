72627925b8778200e2aab31c55c23d803480dcb6
stult <254298804+Valerian996@users.noreply.github.com>
2026-06-13 21:00:37 +0300

    update hello.py

--- hello.py
+++ hello.py
@@ -8,5 +8,10 @@ players = [
 
 best_player = max(players, key=lambda p: p["kills"])
 worst_player = min(players, key=lambda p: p["kills"])
+avg_kills = sum(p["kills"] for p in players) / len(players)
+top_players = sorted(players, key=lambda p: p["kills"], reverse=True)
 
+for p in top_players[:3]:
+    print(f"{p['name']} - {p['kills']}")
 print(f"Best player: {best_player['name']} ({best_player['kills']} kills) and Worst player: {worst_player['name']} ({worst_player['kills']} kills)")
+print(f"Average kills: {avg_kills}")
