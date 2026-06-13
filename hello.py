181d2f68836d115772a67669f3853860023e673a
stult <254298804+Valerian996@users.noreply.github.com>
2026-06-13 20:16:48 +0300

    update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello Git")
+#print("Hello Git")
+
+players = [
+    {"name": "Player1", "kills": 20},
+    {"name": "Player2", "kills": 15},
+    {"name": "Player3", "kills": 30}
+]
+
+best_player = max(players, key=lambda p: p["kills"])
+
+print(f"Best player: {best_player['name']} ({best_player['kills']} kills)")
