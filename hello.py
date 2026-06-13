f3c4e0325e05bf9ed73f4d485210bf90bd3e7e06
stult <254298804+Valerian996@users.noreply.github.com>
2026-06-13 21:32:44 +0300

    update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #print("Hello Git")
-
+import json
 players = [
     {"name": "Player1", "kills": 20},
     {"name": "Player2", "kills": 15},
@@ -9,9 +9,11 @@ players = [
 best_player = max(players, key=lambda p: p["kills"])
 worst_player = min(players, key=lambda p: p["kills"])
 avg_kills = sum(p["kills"] for p in players) / len(players)
+
 top_players = sorted(players, key=lambda p: p["kills"], reverse=True)
 
 for p in top_players[:3]:
     print(f"{p['name']} - {p['kills']}")
+
 print(f"Best player: {best_player['name']} ({best_player['kills']} kills) and Worst player: {worst_player['name']} ({worst_player['kills']} kills)")
-print(f"Average kills: {avg_kills}")
+print(f"Average kills: {avg_kills:.3f}")
